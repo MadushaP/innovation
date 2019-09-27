c53cdc2efc383a647251252707fb227a31b614b3
Madusha <madusha.pallawela@bbc.co.uk>
2019-09-27 16:08:09 +0100

    Added drag and drop zone

---
+++ computer-vision/computer-vision-app/src/StyledDropzone.js
@@ -0,0 +1,113 @@
+import React, {useEffect, useState} from 'react';
+import { useDropzone } from 'react-dropzone';
+import styled from 'styled-components';
+
+const getColor = (props) => {
+  if (props.isDragAccept) {
+    return '#00e676';
+  }
+  if (props.isDragReject) {
+    return '#ff1744';
+  }
+  if (props.isDragActive) {
+    return '#2196f3';
+  }
+  return '#eeeeee';
+}
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  height: 50px;
+  border-width: 2px;
+  border-radius: 2px;
+  border-color: ${props => getColor(props)};
+  border-style: dashed;
+  background-color: #fafafa;
+  color: #bdbdbd;
+  outline: none;
+  transition: border .24s ease-in-out;
+  margin: 0 auto;
+  position: absolute;
+  left: 40%;
+  top: 10%;
+`;
+
+
+const thumbsContainer = {
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  marginTop: 16
+};
+
+const thumb = {
+  display: 'inline-flex',
+  borderRadius: 2,
+  border: '1px solid #eaeaea',
+  margin: '0 auto',
+  width: 500,
+  height: 300,
+  boxSizing: 'border-box',
+  position: 'absolute',
+  left: '38%',
+  top: '20%'
+};
+
+const thumbInner = {
+  display: 'flex',
+  minWidth: 0,
+  overflow: 'hidden'
+};
+
+const img = {
+  display: 'block',
+  width: '100%',
+  height: '100%'
+};
+
+
+export default function StyledDropzone(props) {
+  const [files, setFiles] = useState([]);
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject
+  } = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedFiles => {
+      setFiles(acceptedFiles.map(file => Object.assign(file, {
+        preview: URL.createObjectURL(file)
+      })));
+    }
+  });
+
+    
+  const thumbs = files.map(file => (
+    <div style={thumb} key={file.name}>
+      <div style={thumbInner}>
+        <img
+          src={file.preview}
+          style={img}
+        />
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="container">
+      <Container {...getRootProps({ isDragActive, isDragAccept, isDragReject })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </Container>
+
+      <aside style={thumbsContainer}>
+        {thumbs}
+      </aside>
+    </div>
+  );
+}
