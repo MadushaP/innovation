93e011bd564ba0cff76310c6d076f63cd3c2d6df
Madusha <madusha.pallawela@bbc.co.uk>
2019-10-04 15:55:03 +0100

    Added pill styling for label

--- computer-vision/computer-vision-app/src/App.js
+++ computer-vision/computer-vision-app/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import StyledDropzone from './StyledDropzone.js'
 
-  
 function App() {
   return (
     <div className="App">
--- computer-vision/computer-vision-app/src/Label.js
+++ computer-vision/computer-vision-app/src/Label.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Badge } from 'reactstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Label(props) {
     return (
@@ -7,7 +9,11 @@ export default function Label(props) {
                 <tbody>
                     <tr >
                         {props.predictResults.map(result => (
-                            <td key={result.id}>{result.name}</td>
+                            <td key={result.id}>
+                            <Badge pill color="success">
+                            {result.name}
+                            </Badge>
+                            </td>
                         ))} 
                    </tr>
                 </tbody>
